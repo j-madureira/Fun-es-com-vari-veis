3c16a430acde118ae8d5b99da3a468c962cd194d
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-21 09:29:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Crie uma função para o usuário digitar algo
+*/
+
+alert(`Bem vindo, digite alguma coisa!`);
+
+function digitar(digito){
+    var digitado = digito;
+    console.log(digitado);
+}
+digitar(prompt(`Digite alguma coisa`));
